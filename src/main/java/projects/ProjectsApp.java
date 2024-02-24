8bb66c71006fb04a7fe7f9f8f7be09c029e4f022
Alec Nelson <nelson1033@outlook.com>
2024-02-24 14:12:48 -0700

    first commit

---
+++ src/main/java/projects/ProjectsApp.java
@@ -0,0 +1,14 @@
+package projects;
+
+import java.sql.Connection;
+import projects.dao.DbConnection;
+
+public class ProjectsApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 projects.dao.DbConnection.getConnection();
+
+	}
+
+}
